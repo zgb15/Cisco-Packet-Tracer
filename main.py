855bb6f0285df22bf27fc2bad1e265962216de6a
Zoe González Blanco <zgbibs@gmail.com>
2022-02-24 13:29:39 +0100

    ciscoPacketTracer

---
+++ main.py
@@ -0,0 +1,6 @@
+print("ENSLISH: Network done by me using Cisco Packet Tracer")
+print("-> Directory contains: .pkt file from Cisco, document where all subnetting was done and packet tracer "
+      "screenshot")
+print("ESPAÑOL: Red montada por mí en Cisco Packet Tracer")
+print("-> Contenido del directorio: archivo .pkt de Cisco, PDF con el enunciado y el subnetting y una captura de "
+      "la red en packet tracer")
